051c9438f2c82625bdf0ab0c3e9add3569162fb0
dormirMais <soonhak1212@naver.com>
2023-02-08 13:32:57 +0900

    [add] vido 클래스 완성2

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import { ImgaeComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponent } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
@@ -8,6 +9,8 @@ class App {
         this.page.attachTo(appRoot);
         const image = new ImgaeComponent("image title", "https://picsum.photos/600/300");
         image.attachTo(appRoot, "beforeend");
+        const video = new VideoComponent("video title", "https://www.youtube.com/watch?v=8P-EhBzjKj0");
+        video.attachTo(appRoot, "beforeend");
         const note = new NoteComponent("note Title", "note Body");
         note.attachTo(appRoot, "beforeend");
         const todo = new TodoComponent("todo Title", "todo Item");
