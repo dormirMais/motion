fca3ace6099ef947ebe3cb0161f0a5c37f4e16b8
dormirMais <soonhak1212@naver.com>
2023-02-08 15:26:33 +0900

    [Add] remove item 추가

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -13,10 +13,15 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
+type OnCloseListener = () => void;
 
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body"></section>
@@ -25,11 +30,19 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
             </div>
           </li>
   `);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(child: Component) {
     //child는 Component를 구현하는 어떤 class든 받을 수 있다는 의미... 이 부분이 살짝 이해하기 어려웠다...
     const container = this.element.querySelector(".page-item__body")! as HTMLElement;
     child.attachTo(container);
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
